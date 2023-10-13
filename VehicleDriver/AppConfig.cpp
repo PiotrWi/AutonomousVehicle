e0afd3a5bb428ab74e645b8a1662e0c051efab69
PiotrWi <tarzanek2@o2.pl>
2023-10-13 13:40:21 +0200

    -Add change to 640x480

--- VehicleDriver/AppConfig.cpp
+++ VehicleDriver/AppConfig.cpp
@@ -6,8 +6,8 @@ namespace
 // TODO(to be moved to file)
 const std::string homeDir = "/home/pioter";
 const std::string CameraDumpDir = homeDir + "/tmp";
-const std::string LeftCoefficientFileLoc = homeDir + "/proj/AuthonomousVehicle/VehicleDriver/Data/instrincts_left.xml";
-const std::string RightCoefficientFileLoc = homeDir + "/proj/AuthonomousVehicle/VehicleDriver/Data/instrincts_right.xml";
+const std::string LeftCoefficientFileLoc = homeDir + "/proj/AuthonomousVehicle/VehicleDriver/Data/instrincts_left640x480.xml";
+const std::string RightCoefficientFileLoc = homeDir + "/proj/AuthonomousVehicle/VehicleDriver/Data/instrincts_right640x480.xml";
 const int cameraFps = 10;
 const int cameraWidth = 640;
 const int cameraHeight = 480;
